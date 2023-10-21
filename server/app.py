08df9962715d7c3624b473fe89a9cf49793fe363
danwycliff <danwycliff6769@gmail.com>
2023-10-21 10:56:59 +0300

    codes

--- server/app.py
+++ server/app.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 from flask import Flask, jsonify, request, make_response
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
@@ -17,10 +15,48 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    def get(self):
+        plants = Plant.query.all()
+
+        plants_dict = [plant.to_dict() for plant in plants]
+
+        response = make_response(
+            jsonify(plants_dict),
+            200
+        )
+        return response
+    def post(self):
+        data = request.get_json()
+        plant = Plant(
+            name = data.get('name'),
+            image= data.get('image'),
+            price =data.get('price')
+        )
+        db.session.add(plant)
+        db.session.commit()
+
+        response_dict = plant.to_dict()
+
+        response = make_response(
+        jsonify(response_dict),
+        201,)
+        return response
+
+api.add_resource(Plants, '/plants')
 
 class PlantByID(Resource):
-    pass
+    def get(self,id):
+        plants = Plant.query.filter_by(id=id).first().to_dict()
+
+        #plants_dict = [plant.to_dict() for plant in plants]
+
+        response = make_response(
+            jsonify(plants),
+            200
+        )
+        return response
+
+api.add_resource(PlantByID, '/plants/<int:id>')
         
 
 if __name__ == '__main__':
